0154efd538567aae33ab56bdd79498a03ef357a4
Brandon Cho <hjun1114@gmail.com>
2017-03-01 04:01:57 -0500

    added remove function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-window.addEventListener('keydown', function(e) {
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+function removeTransition (e) {
+  if (e.propertyName !== 'transform'){ return; }
+  this.classList.remove('pressed');
+}
+
+function play(e) {
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // let and const are alternatives to var
+  // Both let and const declare local variables with block scope rather than function scope
   key.classList.toggle("pressed");
-});
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', play);
